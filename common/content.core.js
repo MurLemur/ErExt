6bdbb64b91a9abbe9240ab9229df269298849779
MurLemur <mirex@ukr.net>
2014-05-06 21:55:56 +0300

    add map tracing option to OVL,OPP,ODL

--- common/content.core.js
+++ common/content.core.js
@@ -375,10 +375,14 @@ if (myoptions.keyalt) {
 			scr.text = scr.text + "(" +
 				(function() {
 					var chest = {};
-					chest.sector = []; // двумерный массив прошедших секторов
+					chest.sectorOK = []; // двумерный массив прошедших секторов OK
 					chest.sectoraliens = [];
+					chest.sectorOVL = [];
+					chest.sectorOPP = [];
+					chest.sectorODL = [];
 					$("#span_mode5").children().on("click", function() {
 						if (chest.sectoraliens.length > 0) chest.sectoraliens = [];
+						if (chest.sectorODL.length > 0) chest.sectorODL = [];
 					})
 
 					// поиск в массиве
@@ -395,7 +399,12 @@ if (myoptions.keyalt) {
 						return -1;
 					}
 					//  координаты 
-					chest.getSectorPosition = function(x, y) {
+					chest.getSectorPosition = function(x, y, newmap) {
+					if (newmap) {
+						Point = main.Map.globalCoordsToRelative(main.Map.getCellGlobalCoords(x, y))
+						PointX = Point.x;
+						PointY = Point.y;
+					} else {
 						var dx, dy;
 						if (x && y) {
 							dx = (main.px - x) * 64 - (Math.abs(main.py - y) % 2) * 32 + (Math.abs(main.py - y) % 2) * (main.py % 2) * 64;
@@ -403,25 +412,28 @@ if (myoptions.keyalt) {
 							PointX = main.CenterX - dx;
 							PointY = main.CenterY - dy;
 						}
+					}
 						return 'top:' + PointY + 'px; left:' + PointX + 'px;';
 					}
-					chest.init = function() {
-						var map_div = $('#main').contents().find('div#container #map_div');
+					chest.init = function(map_mas,cur_mas,newmap) {
+						if (newmap) {
+							var map_div = $('#main').contents().find('div#container #mapContent'); 
+						}	
+						else {
+							var map_div = $('#main').contents().find('div#container #map_div');
+						}
 
-						if (map_div && main.map) {
+						if (map_div && map_mas) {
 							var overlay = $('<div id="overlay"></div>');
 							map_div.append(overlay);
 							overlay.attr('style', map_div.find('div:first').attr('style')).css({
 								zIndex: '3'
 							});
 
-							if (user.place2 == 8)
-								var cur_mas = chest.sector;
-							else if ((user.place2 > 19) && (user.place2 < 100))
-								var cur_mas = chest.sectoraliens;
-							for (i = 0; i < main.map.length; i++) {
-								if (chest.search(main.map[i][0] + ':' + main.map[i][1], cur_mas) != -1)
-									overlay.append('<div class="point activ" style="position:absolute; ' + chest.getSectorPosition(main.map[i][0], main.map[i][1]) + '"><span>' + main.map[i][0] + ':' + main.map[i][1] + '</span></div>');
+					
+							for (i = 0; i < map_mas.length; i++) {
+								if (chest.search(map_mas[i][0] + ':' + map_mas[i][1], cur_mas) != -1)
+									overlay.append('<div class="point activ" style="position:absolute; ' + chest.getSectorPosition(map_mas[i][0], map_mas[i][1],newmap) + '"><span>' + map_mas[i][0] + ':' + map_mas[i][1] + '</span></div>');
 							}
 
 							overlay.find('.point.activ').css({
@@ -445,15 +457,34 @@ if (myoptions.keyalt) {
 					chest.map_trace_handler = function() {
 						// если сектора нет в массиве и map определена (main.map работает на ОК, альенах и КТ вроде бы, так что может еще какую то проверку не знаю)
 
-						if (user.place2 == 8)
-							var current_mas = chest.sector;
-						else if ((user.place2 > 19) && (user.place2 < 100))
-							var current_mas = chest.sectoraliens;
-						else return;
+					if (user.place2 == 8) {
+						var current_mas = chest.sectorOK;
+						var map_mas = main.map;
+						var newmap = false;
+					} else if (user.place2 == 1) {
+						var current_mas = chest.sectorOVL;
+						var map_mas = main.Map.sectors;
+						var newmap = true;
+						main.sector = main.Map.currentSector.x + ':' + main.Map.currentSector.y;
+					} else if (user.place2 == 3) {
+						var current_mas = chest.sectorOPP;
+						var map_mas = main.Map.sectors;
+						var newmap = true;
+						main.sector = main.Map.currentSector.x + ':' + main.Map.currentSector.y;
+					} else if (user.place2 > 99) {
+						var current_mas = chest.sectorODL;
+						var map_mas = main.Map.sectors;
+						var newmap = true;
+						main.sector = main.Map.currentSector.x + ':' + main.Map.currentSector.y;
+					} else if ((user.place2 > 19) && (user.place2 < 100)) {
+						var current_mas = chest.sectoraliens;
+						var map_mas = main.map;
+						var newmap = false;
+					} else return;
 
-						if (chest.search(main.sector, current_mas) == -1 && main.map)
+						if (chest.search(main.sector, current_mas) == -1 && map_mas)
 							current_mas.push(main.sector);
-						chest.init();
+						chest.init(map_mas,current_mas,newmap);
 					}
 
 					$("img[src*=footprint]").on('click', function() {
