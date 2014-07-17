2d0350bf2a5870f73c092a0d7f4a0caf4f30ccfb
MurLemur <mirex@ukr.net>
2014-07-17 15:41:51 +0300

    fix estate

--- common/content.estate.js
+++ common/content.estate.js
@@ -17,25 +17,22 @@
 
 //================================================================Begin
 
-/*
 kango.invokeAsync('kango.storage.getItem', "options", function(value) {
 	myoptions = mergeOptions(value, defaultConfig.myoptions);
 
 	if (!myoptions.unpaused) {
 		return;
 	}
-	//=====================================================================  
+	//=====================================================================
 
-	var scr = document.createElement("script");
-	scr.text = "";
+	script = "";
 
-	
-	var veto_administracii = true; // В надежде что когда то администрация разрешит сворачивать диалоги поместья
+	var veto_administracii = false; // В надежде что когда то администрация разрешит сворачивать диалоги поместья
 
-	if (veto_administracii) {
+	if (!veto_administracii) {
 
 
-		scr.text = scr.text + "(" +
+		script = script + "(" +
 			(function() {
 
 			var zzz = Estates.parseDialog;
@@ -51,7 +48,7 @@ kango.invokeAsync('kango.storage.getItem', "options", function(value) {
 				if ($("#mydialoglink").length) {
 					$("#mydialoglink").show();
 				} else {
-					document.getElementById("estateNameButton").parentNode.innerHTML = '' + document.getElementById("estateNameButton").parentNode.innerHTML + '<a id="mydialoglink" href="javascript: Estates.UnhideDialog();">[+]<a>';
+					$("#estateNameButton").parent().append($("<a id=\"mydialoglink\" href=\"javascript: Estates.UnhideDialog();\">[+]</a>"));
 				}
 			};
 
@@ -68,33 +65,55 @@ kango.invokeAsync('kango.storage.getItem', "options", function(value) {
 			};
 
 			Estates.parseDialog = function() {
-				var mdialog = zzz(arguments[0]); 
-
-				if (mdialog.type == "fightattackclose") {
-					var nn = mdialog.text.indexOf('</b><br /><br />Базовый шанс:');
-					var mname = mdialog.text.substring(39, nn);
-					mlink = "<a href='http://www.ereality.ru/~" + mname + "' target='_blank'><b>" + mname + "</b></a>"
-					mdialog.text = mdialog.text.split("<b>" + mname + "</b>").join(mlink);					
-				}
-				mdialog.title += '<a href="javascript: Estates.hideDialog();">[X]<a>';
+				var mdialog = zzz(arguments[0]);
+		
+				mdialog.title = mdialog.title + '<a href="javascript: Estates.hideDialog();">[X]<a>';
 				if ((mdialog.type == "selection") && ($("#estateSelectionCard1")[0].previousElementSibling.innerHTML.search("[X]") < 0)) {
-					$("#estateSelectionCard1")[0].previousElementSibling.innerHTML += '<a href="javascript: Estates.hideDialog();">[X]<a>';
+					$("#estateSelectionCard1").prev().append($("<a href=\"javascript: Estates.hideDialog();\">[X]</a>"));
 				}
-
 				return mdialog;
 			}
 
 		}).toString() + ")();";
 	}
+
+	script = script + "(" +
+		(function() {
+
+		var zzz = Estates.showAttackHistory;
+
+		Estates.showAttackHistory = function() {
+			zzz.apply(Estates);
+			$.each($("#estateHistoryData b"), function(num, val) {
+				$(val).append($("<a href='http://www.ereality.ru/~" + val.innerHTML + "' target='_blank'><img class=\"inf\" src=\"http://img.ereality.ru/inf.gif\"></a>"));
+			})
+			return;
+		}
+
+	}).toString() + ")();";
+
+	//Обновка инфы для таймера поместья
+	if (myoptions.timer_estate) {
+		script = script + "(" +
+			(function() {
+			var OldprocessResponse = Estates.processResponse;
+			Estates.processResponse = function(xml) {
+				var myrezult = OldprocessResponse.apply(Estates, arguments);
+				top.core.mur_timer.estate_getinfo();
+				return myrezult
+			}
+
+		}).toString() + ")();";
+	}
+
+	inject_global(script);
+
+
 	
 
-	document.body.appendChild(scr);
-	//=========================end.
 });
-*/
 
 function controller(extOptions) {	
-	var scr = document.createElement("script");
 	var imagesOptions = {
 		estateVictimListSmall: estateUiBuilderCss.listImgSmall
 	}
@@ -120,7 +139,7 @@ function controller(extOptions) {
 			return mdialog;
 		}
 		
-		var oldShowAttackHistory = Estates.showAttackHistory;
+		/*var oldShowAttackHistory = Estates.showAttackHistory;
 		var estateHistoryHolder = $('#estateHistoryData');
 		Estates.showAttackHistory = function() {
 			oldShowAttackHistory.apply(Estates);
@@ -131,6 +150,7 @@ function controller(extOptions) {
 				element.html("<a href='http://www.ereality.ru/~" + userName + "' target='_blank'><b>" + userName + "</b></a>");
 			});
 		}
+		*/
 			
 	}).toString()
 		.replace("optionsReplace", '(' + JSON.stringify(extOptions) + ')')
