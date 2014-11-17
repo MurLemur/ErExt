d94797913582ec48369e43b37d047332a06e7adb
igorez <igor@eztexting.com>
2014-11-17 21:49:41 +0200

    Norb:
    Refactoring footstep

--- common/scripts/core_map_trace.js
+++ common/scripts/core_map_trace.js
@@ -1,70 +1,132 @@
 var script_map_trace = "(" +
 	(function() {
-		var chest = {};
-		chest.sectorOK = []; // двумерный массив прошедших секторов OK
-		chest.sectoraliens = [];
-		chest.sectorOVL = [];
-		chest.sectorOPP = [];
-		chest.sectorODL = [];
-		chest.sectorTO = [];
-		$("#span_mode5").children().on("click", function() {
-			if (chest.sectoraliens.length > 0) chest.sectoraliens = [];
-			if (chest.sectorODL.length > 0) chest.sectorODL = [];
-		})
+		var erExtMainFraime = $('#main');
+		
+		var _chest = function(mainFraime) {
+			this.mainFraime = mainFraime;
+			this.maps = {
+				'ok': {
+					overlay: null, 
+					footsteps: {},
+					mapNew: false 
+				},
+				'aliens': {
+					overlay: null,
+					footsteps: {},
+					mapNew: false 
+				},
+				'ovl': {
+					overlay: null,
+					footsteps: {},
+					mapNew: true
+				},
+				'opp': {
+					overlay: null,
+					footsteps: {},
+					mapNew: true
+				},
+				'odl': {
+					overlay: null,
+					footsteps: {},
+					mapNew: false
+				},
+				'to': {
+					overlay: null,
+					footsteps: {},
+					mapNew: true
+				}
+			};
+			
+			var self = this;
+			
+			this.drawOverlay = function(x, y, mapID) {
+				if (self.maps[mapID].mapNew) {
+					var map_div = self.mainFraime.contents().find('div#container #mapContent');
+				} else {
+					var map_div = self.mainFraime.contents().find('div#container #map_div');
+				}
 
-		// поиск в массиве
-		chest.search = function(s, mas) {
-			if (mas.indexOf) { // если метод существует
-				return mas.indexOf(s);
-			}
+				if (map_div.length == 0) {
+					return;
+				}
 
+				var overlay = self.getOverlay(mapID);
+				
+				self.addFootstep(x, y, mapID, overlay);
+				self.moveFootstepsToPositions(overlay, mapID);
+				
+				map_div.append(overlay);
+			};
+			
+			this.getOverlay = function(mapID) {
+				if (self.maps[mapID]['overlay'] == null) {
+					self.maps[mapID]['overlay'] = self.initOverlay();
+				}
+				
+				return self.maps[mapID]['overlay'];
+			};
+			
+			this.initOverlay = function() {
+				return $('<div></div>');
+			};
 
-			for (var i = 0; i < mas.length; i++) {
-				if (mas[i] === s) return i;
-			}
-
-			return -1;
-		}
-		//  координаты 
-		chest.getSectorPosition = function(x, y, newmap) {
-			if (newmap) {
-				Point = main.Map.globalCoordsToRelative(main.Map.getCellGlobalCoords(x, y))
-				PointX = Point.x;
-				PointY = Point.y;
-			} else {
-				var dx, dy;
-				if (x && y) {
-					dx = (main.px - x) * 64 - (Math.abs(main.py - y) % 2) * 32 + (Math.abs(main.py - y) % 2) * (main.py % 2) * 64;
-					dy = (main.py - y) * 16;
+			this.getSectorPosition = function(x, y, newmap) {
+				if (newmap) {
+					Point = main.Map.globalCoordsToRelative(main.Map.getCellGlobalCoords(x, y))
+					PointX = Point.x;
+					PointY = Point.y;
+				} else {
+					var dx = (main.px - x) * 64 - (Math.abs(main.py - y) % 2) * 32 + (Math.abs(main.py - y) % 2) * (main.py % 2) * 64;
+					var dy = (main.py - y) * 16;
 					PointX = main.CenterX - dx;
 					PointY = main.CenterY - dy;
 				}
-			}
-			return 'top:' + PointY + 'px; left:' + PointX + 'px;';
-		}
-		chest.init = function(map_mas, cur_mas, newmap) {
-			if (newmap) {
-				var map_div = $('#main').contents().find('div#container #mapContent');
-			} else {
-				var map_div = $('#main').contents().find('div#container #map_div');
-			}
-
-			if (map_div && map_mas) {
-				var overlay = $('<div id="overlay"></div>');
-				map_div.append(overlay);
-				overlay.attr('style', map_div.find('div:first').attr('style')).css({
-					zIndex: '3'
+				return {
+					X: PointX,
+					Y: PointY
+				};			
+			};
+			
+			this.moveFootstepsToPositions = function(overlay, mapID) {
+				overlay.children().each(function() {
+					var footstep = $(this);
+					
+					var sectorPositions = self.getSectorPosition(footstep.attr('sectorX'), footstep.attr('sectorY'), self.maps[mapID].mapNew);
+					footstep.css({
+						left: sectorPositions.X,
+						top: sectorPositions.Y,
+					});
 				});
-
-
-				for (i = 0; i < map_mas.length; i++) {
-					if (chest.search(map_mas[i][0] + ':' + map_mas[i][1], cur_mas) != -1)
-						var expr = new RegExp(">"+map_mas[i][0] + ':' + map_mas[i][1]+"<");
-						if (overlay.html().search(expr)==-1)
-						overlay.append('<div class="point activ" style="position:absolute; ' + chest.getSectorPosition(map_mas[i][0], map_mas[i][1], newmap) + '"><span>' + map_mas[i][0] + ':' + map_mas[i][1] + '</span></div>');
+			};
+			
+			this.addFootstep = function(x, y, mapID, overlay) {
+				if (self.isFootstepExists(x, y, mapID)) {
+					return;
 				}
-
-				overlay.find('.point.activ').css({
+				
+				overlay.append(self.getFootprint(x, y, self.maps[mapID].mapNew));				
+				self.saveFootstep(x, y, mapID);
+			};
+			
+			this.isFootstepExists = function(x, y, mapID) {
+				if (typeof self.maps[mapID]['footsteps'][x] == 'undefined' || typeof self.maps[mapID]['footsteps'][x][y] == 'undefined') {
+					return false;
+				}
+				
+				return self.maps[mapID]['footsteps'][x][y];
+			};
+			
+			this.saveFootstep = function(x, y, mapID) {
+				if (typeof self.maps[mapID]['footsteps'][x] == 'undefined') {
+					self.maps[mapID]['footsteps'][x] = [];
+				}
+				
+				self.maps[mapID]['footsteps'][x][y] = true;
+			};
+			
+			this.getFootprint = function(positionX, positionY, newMap) {
+				return $('<div sectorX="' + positionX + '" sectorY="' + positionY + '"><span>' + positionX + ':' + positionY + '</span></div>').css({
+					position: 'absolute',
 					height: '32px',
 					width: '64px',
 					lineHeight: '32px',
@@ -74,73 +136,78 @@ var script_map_trace = "(" +
 					fontWeight: 'bold',
 					opacity: '0.6',
 					color: '#222',
-					textShadow: '0 0 5px #eee'
-				}).css('background-image', 'url(sec_red.png)');
-				return true;
-			} else {
-				return false;
+					textShadow: '0 0 5px #eee',
+					'background-image': 'url(sec_red.png)'
+				});
+			};
+			
+			this.userMadeFootstep = function() {
+				var mapType = null;
+				
+				if (user.coldX != "0,0") {
+					mapType = 'odl';
+				} else if (user.place2 == 8) {
+					mapType = 'ok';
+				} else if (user.place2 == 1) {
+					mapType = 'ovl';
+				} else if (user.place2 == 3) {
+					mapType = 'opp';
+				} else if (user.place2 == 14) {
+					mapType = 'to';
+				} else if ((user.place2 > 19 && user.place2 < 100)  || (user.place2 > 199 && user.place2 < 310)) {
+					mapType = 'aliens';
+				}
+				
+				if (mapType == null) {
+					return;
+				}
+				
+				self.drawOverlay(main.px, main.py, mapType);
+			};
+			
+			this.clearOverlay = function(mapID) {
+				self.maps[mapID]['footsteps'] = {};
+				self.maps[mapID]['overlay'] = null;
 			}
-		}
-
-		chest.map_trace_handler = function() {
-			// если сектора нет в массиве и map определена (main.map работает на ОК, альенах и КТ вроде бы, так что может еще какую то проверку не знаю)
-
-			if (user.coldX != "0,0") {
-				var current_mas = chest.sectorODL;
-				var map_mas = main.map;
-				var newmap = false;
-			} else if (user.place2 == 8) {
-				var current_mas = chest.sectorOK;
-				var map_mas = main.map;
-				var newmap = false;
-			} else if (user.place2 == 1) {
-				var current_mas = chest.sectorOVL;
-				var map_mas = main.Map.sectors;
-				var newmap = true;
-			} else if (user.place2 == 3) {
-				var current_mas = chest.sectorOPP;
-				var map_mas = main.Map.sectors;
-				var newmap = true;
-			} else if (user.place2 == 14) {
-				var current_mas = chest.sectorTO;
-				var map_mas = main.Map.sectors;
-				var newmap = true;
-			} else if (((user.place2 > 19) && (user.place2 < 100))  || ((user.place2 > 199) && (user.place2 < 310))) {
-				var current_mas = chest.sectoraliens;
-				var map_mas = main.map;
-				var newmap = false;
-
-				//Следы для тиммейтов
+			
+			// не переписывал, пока выключенно почему-то
+			this.footstepsForTeammate = function() {
+				/*	
 				var teammate_trace = false;
-				if (teammate_trace)
-					if (user.place2 == 14) 
-					$.each(main.Map.additionalObjects, function(key) {
-						var msector = key;
-						if (chest.search(msector, current_mas) == -1 && map_mas)
-							current_mas.push(msector);
-					});	
-					else
-					$.each(main.placeHeroes, function(key, places) {
-						var msector = places.x + ':' + places.y;
-						if (chest.search(msector, current_mas) == -1 && map_mas)
-							current_mas.push(msector);
-					});
-
-			} else return;
-			msector = main.px + ':' + main.py;
-			if (chest.search(msector, current_mas) == -1 && map_mas)
-				current_mas.push(msector);
-			chest.init(map_mas, current_mas, newmap);
-		}
-
+					if (teammate_trace) {
+						if (user.place2 == 14) {
+							$.each(main.Map.additionalObjects, function(key) {
+								var msector = key;
+								if (chest.search(msector, current_mas) == -1 && map_mas)
+									current_mas.push(msector);
+							});	
+						} else {
+							$.each(main.placeHeroes, function(key, places) {
+								var msector = places.x + ':' + places.y;
+								if (chest.search(msector, current_mas) == -1 && map_mas)
+									current_mas.push(msector);
+							});
+						}						
+					}
+				*/
+			};
+		};
+		
+		var chest = new _chest(erExtMainFraime);
+		
+		$("#span_mode5").children().on("click", function() {
+			chest.clearOverlay('aliens');
+			chest.clearOverlay('odl');
+		});				
+		
 		$("img[src*=footprint]").on('click', function() {
 			setTimeout(
 				function() {
 					if ($("img[src*=footprint_on]").length == 1) {
 						// привязываем событие
-						$('#main').on('load.chest', chest.map_trace_handler);
+						erExtMainFraime.on('load', chest.userMadeFootstep);
 					} else {
-						$('#main').off('load.chest', chest.map_trace_handler);
+						erExtMainFraime.off('load', chest.userMadeFootstep);
 					}
 				}, 100);
 
